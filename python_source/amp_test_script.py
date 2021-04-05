79c273031d16533c4b34fb90a9502665ff60a2ba
fluorine21 <james.an.williams@gmail.com>
2021-04-05 14:29:47 -0700

    Debugged amp testing script

--- python_source/amp_test_script.py
+++ python_source/amp_test_script.py
@@ -1,23 +1,25 @@
 
+
 import rfsoc_board_driver as rbd
 
-portname = "COM4"
+portname = "COM15"
 board_obj = rbd.rfsoc_board(portname)
 
+scale = 0.5
 
 #generate our test waveform for a single pulse
 
-wave_single_pulse = [rbd.DAC_MAX_VALUE]*8 + [0]*8 + [0] * 64
+wave_single_pulse = [round(rbd.DAC_MAX_VALUE * scale)]*8 + [0]*8 + [0] * 64
 single_pulse_cycles = round(len(wave_single_pulse)/16)
 
 #Set up our test channel
-chan = rbd.rfsoc_channel("DAC", 0, 0, 0, 0, 0, 8, 1, 1, "", "", 0, 0)
+chan = rbd.rfsoc_channel("DAC", 4, 0, 0, 0, 0, 8, 1, 1, "", "", 0, 0)
 chan.waveform_samples = wave_single_pulse
 chan.run_cycles = single_pulse_cycles
 
 
 #Add the test channel to the board object
-board_obj.channel_list.append(chan)
+board_obj.channel_list = [chan]
 
 #Configure the board
 if(board_obj.configure_all_channels()):
@@ -36,8 +38,9 @@ if(board_obj.trigger()):
 
 wave_ramp = []
 
-for i in range(100, 1):
-    wave_ramp += [round(rbd.DAC_MAX_VALUE*(i/100))] * 8 + [0] * 8
+for i in range(1, 100):
+    v = round(rbd.DAC_MAX_VALUE*((100-i)/100) * scale)
+    wave_ramp += [v] * 8 + [-1*v] * 8
     
 ramp_cycles = round(len(wave_ramp)/16)
 
@@ -49,9 +52,25 @@ chan.run_cycles = ramp_cycles
 if(board_obj.configure_all_channels()):
     raise RuntimeError("Error, could not configure channels for amp test")
 
-print("Press any key to trigger board for single pulse test")
+print("Press any key to trigger board for ramp test")
+
 
 dummy = input()
+
+#Trigger the board once
+if(board_obj.trigger()):
+    raise RuntimeError("Error triggering board")
+    
+    
+chan.locking_waveform_samples = [round(rbd.DAC_MAX_VALUE * scale)]*8
+chan.locking_waveform_samples += [round(rbd.DAC_MAX_VALUE * scale * -1)]*8  
+
+print("Press any key to do continuous pulse test")
+
+dummy = input()
+
+if(board_obj.configure_all_channels()):
+    raise RuntimeError("Error, could not configure channels for amp test")
 
 print("Done!")
 
